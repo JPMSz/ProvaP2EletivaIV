4a1f93fea9aa0896f15c9934bb4b54d26690fa29
JPMSz <jpmds16@gmail.com>
2025-11-25 11:11:55 -0300

    Ultimo commit

--- visualizarjogos.php
+++ visualizarjogos.php
@@ -4,4 +4,12 @@ require "funcoesBD.php";
 
 ?>
 
+<header> Visualizar Jogos Black Friday </header>
+
+<section class="Visualizar">
+    <h2 class="corsecTitulo">Visualize os Jogos</h2>
+      <div class="jogo" style="width: 80%;">
+            <form class="formulario" action="processamento.php" method="post"> 
+
 <footer> João Pedro Martins de Souza - Eletiva Linguagem de Programação IV </footer>
+
